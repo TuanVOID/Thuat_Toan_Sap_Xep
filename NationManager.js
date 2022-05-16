6ad78a300e8133830458349dfe57820e9d7a49b5
TuanVOID <102146396+TuanVOID@users.noreply.github.com>
2022-05-16 17:00:31 +0700

    clean lai

--- NationManager.js
+++ NationManager.js
@@ -11,29 +11,28 @@ var NationManager = /** @class */ (function () {
     NationManager.prototype.getList = function () {
         return this.nations;
     };
+    NationManager.prototype.swap = function (arr, a, b) {
+        // let temp = arr[a]
+        // arr[a] = arr[b]
+        // arr[b] = temp;
+        var _a;
+        _a = [this.nations[b], this.nations[a]], this.nations[a] = _a[0], this.nations[b] = _a[1];
+    };
     NationManager.prototype.LeaderBoard = function () {
         for (var i = 0; i < this.nations.length; i++) {
             for (var j = 0; j < this.nations.length - 1 - i; j++) {
                 if (this.nations[j].gold < this.nations[j + 1].gold) {
-                    var temp = this.nations[j];
-                    this.nations[j] = this.nations[j + 1];
-                    this.nations[j + 1] = temp;
+                    this.swap(this.nations, j, j + 1);
                 }
                 else if (this.nations[j].gold == this.nations[j + 1].gold && this.nations[j].silver < this.nations[j + 1].silver) {
-                    var temp = this.nations[j];
-                    this.nations[j] = this.nations[j + 1];
-                    this.nations[j + 1] = temp;
+                    this.swap(this.nations, j, j + 1);
                 }
                 else if (this.nations[j].gold == this.nations[j + 1].gold && this.nations[j].silver == this.nations[j + 1].silver && this.nations[j].bronze < this.nations[j + 1].bronze) {
-                    var temp = this.nations[j];
-                    this.nations[j] = this.nations[j + 1];
-                    this.nations[j + 1] = temp;
+                    this.swap(this.nations, j, j + 1);
                 }
                 else if (this.nations[j].gold == this.nations[j + 1].gold && this.nations[j].silver == this.nations[j + 1].silver && this.nations[j].bronze == this.nations[j + 1].bronze) {
                     if (this.nations[j].name.localeCompare(this.nations[j + 1].name) > 0) {
-                        var temp = this.nations[j];
-                        this.nations[j] = this.nations[j + 1];
-                        this.nations[j + 1] = temp;
+                        this.swap(this.nations, j, j + 1);
                     }
                 }
             }
--- NationManager.ts
+++ NationManager.ts
@@ -12,26 +12,26 @@ export class NationManager {
         return this.nations
     }
 
+    swap(arr,a,b) {
+        // let temp = arr[a]
+        // arr[a] = arr[b]
+        // arr[b] = temp;
+
+        [this.nations[a],this.nations[b]] = [this.nations[b],this.nations[a]]
+    }
+
     LeaderBoard() {
         for (let i = 0; i < this.nations.length; i++) {
             for (let j = 0; j < this.nations.length - 1 - i; j++) {
                 if (this.nations[j].gold < this.nations[j + 1].gold) {
-                    let temp = this.nations[j]
-                    this.nations[j] = this.nations[j + 1]
-                    this.nations[j + 1] = temp;
+                    this.swap(this.nations,j,j+1)
                 } else if (this.nations[j].gold == this.nations[j + 1].gold && this.nations[j].silver < this.nations[j + 1].silver) {
-                    let temp = this.nations[j]
-                    this.nations[j] = this.nations[j + 1]
-                    this.nations[j + 1] = temp;
+                    this.swap(this.nations,j,j+1)
                 } else if (this.nations[j].gold == this.nations[j + 1].gold && this.nations[j].silver == this.nations[j + 1].silver && this.nations[j].bronze < this.nations[j + 1].bronze) {
-                    let temp = this.nations[j]
-                    this.nations[j] = this.nations[j + 1]
-                    this.nations[j + 1] = temp;
+                    this.swap(this.nations,j,j+1)
                 } else if (this.nations[j].gold == this.nations[j + 1].gold && this.nations[j].silver == this.nations[j + 1].silver && this.nations[j].bronze == this.nations[j + 1].bronze) {
                     if (this.nations[j].name.localeCompare(this.nations[j + 1].name) > 0) {
-                        let temp = this.nations[j]
-                        this.nations[j] = this.nations[j + 1]
-                        this.nations[j + 1] = temp;
+                        this.swap(this.nations,j,j+1)
                     }
                 }
             }
